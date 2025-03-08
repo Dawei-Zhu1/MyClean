db3d651b740c77194cfdb06c1c980c2ad21a98dd
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-03-08 21:16:10 +1000

    Rename the folder

--- webpages/head.php
+++ webpages/head.php
@@ -6,4 +6,7 @@
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/stylesheets/style.css">
     <title><?php echo "$section_name - MyClean" ?? 'MyClean'; ?></title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
 </head>
