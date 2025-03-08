5253f4e5bc4af9e83ff6a0a136a8cfb32749b36e
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-03-08 21:01:28 +1000

    Rename the folder

--- pages/head.php
+++ pages/head.php
@@ -4,5 +4,6 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title><?php echo "$title - MyClean" ?? 'MyClean'; ?></title>
+    <link rel="stylesheet" href="/stylesheets/style.css">
+    <title><?php echo "$section_name - MyClean" ?? 'MyClean'; ?></title>
 </head>
