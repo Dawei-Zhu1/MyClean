deea773fb56ee97d3c73d349b18f44ba37d69ffe
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-03-09 23:17:36 +1000

    Modify the style

--- webpages/register.php
+++ webpages/register.php
@@ -7,34 +7,81 @@ include 'head.php'
 <?php
 include 'navbar.php'
 ?>
-<div id="container">
+<div class="container">
     <div id="reg_form">
-        <h1>Personal Registration</h1>
+        <h1 class="text-center">Personal Registration</h1>
         <form id="registration_form">
             <!--Name-->
-            <label for="fname">Given name</label>
-            <input type="text" id="fname" placeholder="First name"><br/>
-            <label for="mname">Middle name</label>
-            <input type="text" id="mname" placeholder="Middle name"><br/>
-            <label for="lname">Family name</label>
-            <input type="text" id="lname" placeholder="Last name"><br/>
+            <div class="row">
+                <div class="col">
+                    <label for="inputFirstName">Given name</label>
+                    <input type="text" class="form-control" id="inputFirstName" placeholder="First name">
+                </div>
+                <div class="col">
+                    <label for="inputLastName"></label>
+                    <input type="text" class="form-control" id="inputLastName" placeholder="Last name">
+                </div>
+            </div>
             <!--DoB-->
-            <label for="dob">Date of Birth</label>
-            <input type="text" id="dob" placeholder="Last name"><br/>
+            <div class="row">
+                <div class="form-group">
+                    <label for="inputDoB">Date of Birth</label>
+                    <input type="text" class="form-control" id="inputDoB" placeholder="Date of birth">
+                </div>
+            </div>
             <!--Email-->
-            <label for="email">Email</label>
-            <input type="email" id="email" placeholder="abc@example.com"><br/>
+            <div class="row">
+                <div class="form-group">
+                    <label for="InputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp"
+                           placeholder="Enter email">
+                </div>
+            </div>
             <!--Phone-->
-            <label for="phone">Phone Number</label>
-            <input type="text" id="phone" placeholder="0123-456-789"><br/>
+            <div class="form-group">
+                <label for="phone">Phone Number</label>
+                <input type="text" id="phone" class="form-control" placeholder="0123-456-789">
+            </div>
+            <!--Address-->
+            <div class="form-group">
+                <label for="inputAddress">Address</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="1 Mains Road">
+            </div>
+            <div class="form-group">
+                <label for="inputAddress2">Address 2</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <!--City-->
+            <div class="row">
+                <div class="form-group col-md-6">
+                    <label for="inputCity">City</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputState">State</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputZip">Zip</label>
+                    <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
             <!--Password-->
-            <label for="pw">Password</label>
-            <input type="password" id="pw"><br/>
-            <label for="confirm_pw">Confirm Password</label>
-            <input type="password" id="confirm_pw" placeholder="Repeat the password"><br/>
+            <div class="form-group">
+                <label for="inputPassword">Password</label>
+                <input type="password" class="form-control" id="inputPassword">
+            </div>
+            <div class="form-group">
+                <label for="confirm_pw">Confirm Password</label>
+                <input type="password" id="confirm_pw" class="form-control" placeholder="Repeat the password">
+            </div>
             <!--Submit Button-->
             <input type="submit" value="Submit">
         </form>
+
     </div>
 </div>
 </body>
