4a43130669d32874c79bf215f5cf991eb38da600
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-03-23 00:26:47 +1000

    Add this file to connect the AWS database

--- webpages/Database.php
+++ webpages/Database.php
@@ -1,22 +1,27 @@
 <?php
-class Database {
+
+class Database
+{
     private $servername = "db-myclean.cj8k0cuo8rz4.ap-southeast-2.rds.amazonaws.com";
     private $username = "admin";
     private $password = "Success+3407"; // DB PW, actually need IAM
     private $dbname = "MYCLEANDB";
     public $conn;
 
-    public function __construct() {
+    public function __construct()
+    {
         // Create connection
         $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-
         // Check connection
         if ($this->conn->connect_error) {
             die("Connect fail: " . $this->conn->connect_error);
         }
     }
 
-    public function close() {
+    public function close()
+    {
         $this->conn->close();
     }
+
+
 }
