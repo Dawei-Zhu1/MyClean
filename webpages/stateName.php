6459d000287bd009ea33abaa3826f3b739b34369
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-03-23 00:21:29 +1000

    This file is used as a state name list

---
+++ webpages/stateName.php
@@ -0,0 +1,11 @@
+<?php
+$state_names = Array(
+    'NSW' => 'New South Wales',
+    'QLD' => 'Queensland',
+    'SA' => 'South Australia',
+    'TAS' => 'Tasmania',
+    'VIC' => 'Victoria',
+    'WA' => 'Western Australia',
+    'ACT' => 'Australian Capital Territory',
+    'NT' => 'Northern Territory',
+);
