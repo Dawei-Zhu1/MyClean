6410e1917e5733449f01cefaa79697e6f8e4016f
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-03-26 14:42:10 +1000

    Refactor project structure

--- myclean/common/head.php
+++ myclean/common/head.php
@@ -4,7 +4,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="/root/assets/stylesheets/style.css">
+    <link rel="stylesheet" href="/myclean/assets/stylesheets/style.css">
     <title><?php echo "$section_name - MyClean" ?? 'MyClean'; ?></title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
