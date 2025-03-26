2221f137ace022dadf50b5830c3f47737346d5eb
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-03-26 14:32:53 +1000

    Refactor project structure

--- root/common/navbar.php
+++ root/common/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar sticky-top navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="/assets/img/myclean-logo.svg" alt="MyClean Logo" width="120" height="50">
+            <img src="/root/assets/img/myclean-logo.svg" alt="MyClean Logo" width="120" height="50">
         </a>
     </div>
 </nav>
