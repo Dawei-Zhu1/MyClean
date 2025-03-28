e72ed20b0636e21fa7c375142df7f0800a0b66c9
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-03-28 17:16:31 +1000

    Modify the file with a safer syntax

--- myclean/register/user.php
+++ myclean/register/user.php
@@ -1,92 +1,37 @@
-<?php
-require '../Database.php';
-$db = new Database();
-$conn = $db->conn;
-//
-////Error and success message
-//$error = '';
-//$success = '';
-//
-//// Check whether the method is POST
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-// Fetch form
-//    Name
-    $family_name = trim($_POST['family_name'] ?? '');
-    $given_name = trim($_POST['given_name'] ?? '');
-//    Email
-    $email = trim($_POST['email'] ?? '');
-//    Password
-    $password = $_POST['password'] ?? '';
-    echo "Posted";
-}
-//
-//// Verify the input
-//    if (empty($username) || empty($email) || empty($password)) {
-//        $error = 'The username email and must be filled!';
-//    } else {
-//        // Check email format
-//        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-//            $error = 'Invalid email address! ';
-//        } else {
-//            // Check username
-//            $stmt = $conn->prepare("SELECT email FROM USER WHERE username = ?");
-//            $stmt->bind_param("e_mail", $email);
-//            $stmt->execute();
-//            $stmt->store_result();
-//
-//            if ($stmt->num_rows > 0) {
-//                $error = 'The Email address already exists! ';
-//            } else {
-//                // Hash
-//                $password_hash = password_hash($password, PASSWORD_DEFAULT);
-//
-//                // Prevent SQL injection via preprocess
-//                $stmt = $conn->prepare("INSERT INTO USER (first, email, password) VALUES (?, ?, ?);INSERT INTO CREDENTIAL (?)");
-//                $stmt->bind_param("sss", $username, $email, $password_hash);
-//
-//                if ($stmt->execute()) {
-//                    $success = '注册成功！';
-//                    // clear all
-//                    $username = $email = '';
-//                } else {
-//                    $error = '数据库错误: ' . $conn->error;
-//                }
-//            }
-//            $stmt->close();
-//        }
-//    }
-//}
-//?>
 <!DOCTYPE html>
 <html lang="en">
-<?php include '../common/head.php' ?>
+<?php $section_name = "Register";
+require_once '../head.php'
+?>
 <body>
-<?php include '../common/navbar.php' ?>
+<?php require_once '../navbar.php' ?>
 <div class="container">
     <div id="register_mode_choosing">
 
     </div>
     <div id="reg_form">
         <h1 class="text-center">Personal Registration</h1>
-        <form id="registration_form">
+        <form id="registration_form" method="post" action="register.php">
             <!--Name-->
             <div class="row">
                 <div class="col">
                     <label for="inputFirstName">Given name</label>
-                    <input type="text" class="form-control" id="inputFirstName" placeholder="First name"
-                           name="given_name">
+                    <input type="text" name="given_name" class="form-control" id="inputFirstName"
+                           placeholder="First name"
+                    >
                 </div>
                 <div class="col">
                     <label for="inputLastName">Family Name</label>
-                    <input type="text" class="form-control" id="inputLastName" placeholder="Last name"
-                           name="family_name">
+                    <input type="text" name="family_name" class="form-control" id="inputLastName"
+                           placeholder="Last name">
                 </div>
             </div>
             <!--DoB-->
             <div class="row">
                 <div class="form-group">
                     <label for="inputDoB">Date of Birth</label>
-                    <input type="text" class="form-control" id="inputDoB" placeholder="Date of birth">
+                    <input type="text" name="dob" class="form-control" id="inputDoB" placeholder="Date of birth"
+                           pattern="">
                 </div>
             </div>
             <!--Email-->
@@ -100,7 +45,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <!--Phone-->
             <div class="form-group">
                 <label for="phone">Phone Number</label>
-                <input type="text" name="phone" id="phone" class="form-control" placeholder="0123-456-789">
+                <input type="tel" name="phone" id="phone" class="form-control" placeholder="0123-456-789"
+                       pattern="[0-9]{10}">
             </div>
             <!--Address-->
             <div class="form-group">
@@ -122,12 +68,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <label for="inputState">State</label>
                     <select id="inputState" name="state" class="form-control">
                         <?php
-//                        The default
+                        //                        The default
                         echo "<option selected>Choose...</option>";
                         include "../stateName.php";
-//                        Read array
+                        //                        Read array
                         foreach ($state_names as $state) {
-                            echo "<option>$state</option>";}
+                            echo "<option>$state</option>";
+                        }
                         ?>
                     </select>
                 </div>
@@ -149,11 +96,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <input type="submit" class="btn btn-primary" value="Submit">
         </form>
         <?php
-        $hashed_password = password_hash("aa", PASSWORD_DEFAULT);
-        var_dump($hashed_password).'\n';
-        echo '<br />';
-        echo password_verify("aa", $hashed_password)."\n";
-        echo password_verify("aa", $hashed_password);
+        //        $hashed_password = password_hash("aa", PASSWORD_DEFAULT);
+        //        var_dump($hashed_password) . '\n';
+        //        echo '<br />';
+        //        echo password_verify("aa", $hashed_password) . "\n";
+        //        echo password_verify("aa", $hashed_password);
         ?>
     </div>
 </div>
