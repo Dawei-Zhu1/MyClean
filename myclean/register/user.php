d1a610f4449a493f58983ba4fef52f7014d895cd
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-03-29 21:51:36 +1000

    Basic validation created

--- myclean/register/user.php
+++ myclean/register/user.php
@@ -1,3 +1,63 @@
+<?php
+require '../Database.php';
+//require_once '../functions.php';
+//include_once 'register_test.php';
+//$db = new Database();
+//$conn = $db->conn;
+$success_flag = 0;
+$username = $email = $password = $confirm_password = $phone = $email = $address1 = $address2 = $postcode = "";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = trim($_POST["given_name"]);
+    $email = trim($_POST["email"]);
+    $password = trim($_POST["password"]);
+    $confirm_password = trim($_POST["confirm_password"]);
+    $dob = trim($_POST["dob"]);
+    $phone = trim($_POST["phone"]);
+    $email = trim($_POST["email"]);
+    $address1 = trim($_POST["address1"]);
+    $address2 = trim($_POST["address2"]);
+    $city = trim($_POST["city"]);
+    $state = trim($_POST["state"]);
+    $postcode = trim($_POST["postcode"]);
+
+    // Basic Validation
+    if (empty($username) || empty($email) || empty($password) || empty($confirm_password)
+        || empty($phone) || empty($address1) || empty($postcode) || empty($dob)) {
+//        Check null
+        $errors[] = "All fields are required.";
+        if ($password !== $confirm_password) {
+            $errors[] = "Passwords do not match.";
+        }
+        echo "<script>"
+            . "alert('Register unsuccessfully, same page processing');"
+            . "</script>";
+        if ($password !== $confirm_password) {
+            // Check  password
+            $errors[] = "Passwords do not match.";
+        }
+    } else{
+        return ;
+    }
+}
+/**
+ * @param string $input_type
+ * @param string $element_name
+ * @return string
+ */
+function keepContent(string $input_type, string $element_name): string
+{
+//    If postValue exists, get the value, else null
+    $postValue = $_POST[$element_name] ?? null;
+    switch ($input_type) {
+        case 'checked':
+            return '';
+        default:
+            return isset($postValue) ? htmlspecialchars($_POST[$element_name]) : '';
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <?php $section_name = "Register";
@@ -6,32 +66,38 @@ require_once '../head.php'
 <body>
 <?php require_once '../navbar.php' ?>
 <div class="container">
+
     <div id="register_mode_choosing">
 
     </div>
     <div id="reg_form">
         <h1 class="text-center">Personal Registration</h1>
-        <form id="registration_form" method="post" action="register.php">
+        <form id="registration_form" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <!--Name-->
             <div class="row">
                 <div class="col">
                     <label for="inputFirstName">Given name</label>
                     <input type="text" name="given_name" class="form-control" id="inputFirstName"
                            placeholder="First name"
+                           value="<?= keepContent('text', 'given_name') ?>"
                     >
                 </div>
                 <div class="col">
                     <label for="inputLastName">Family Name</label>
                     <input type="text" name="family_name" class="form-control" id="inputLastName"
-                           placeholder="Last name">
+                           placeholder="Last name"
+                           value="<?= keepContent('text', 'family_name') ?>">
+                    <div class="invalid-feedback">
+                        Please enter a message in the textarea.
+                    </div>
                 </div>
             </div>
             <!--DoB-->
             <div class="row">
                 <div class="form-group">
                     <label for="inputDoB">Date of Birth</label>
-                    <input type="text" name="dob" class="form-control" id="inputDoB" placeholder="Date of birth"
-                           pattern="">
+                    <input type="date" name="dob" class="form-control" id="inputDoB" placeholder="Date of birth"
+                           pattern="\d{4}-\d{2}-\d{2}">
                 </div>
             </div>
             <!--Email-->
@@ -61,8 +127,8 @@ require_once '../head.php'
             <!--City-->
             <div class="row">
                 <div class="form-group col-md-6">
-                    <label for="inputCity">City</label>
-                    <input type="text" name="suburb" class="form-control" id="inputCity">
+                    <label for="inputCity">City / Suburb</label>
+                    <input type="text" name="city" class="form-control" id="inputCity">
                 </div>
                 <div class="form-group col-md-4">
                     <label for="inputState">State</label>
@@ -89,8 +155,9 @@ require_once '../head.php'
                 <input type="password" class="form-control" id="inputPassword" name="password">
             </div>
             <div class="form-group">
-                <label for="confirm_pw">Confirm Password</label>
-                <input type="password" id="confirm_pw" class="form-control" placeholder="Repeat the password">
+                <label for="confirm_password">Confirm Password</label>
+                <input type="password" id="confirm_password" class="form-control" name="confirm_password"
+                       placeholder="Repeat the password">
             </div>
             <!--Submit Button-->
             <input type="submit" class="btn btn-primary" value="Submit">
