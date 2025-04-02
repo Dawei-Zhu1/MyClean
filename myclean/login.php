269834328e6f6a8346048ef97614909cf4faa2aa
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-02 18:30:15 +1000

    Implement verification function

--- myclean/login.php
+++ myclean/login.php
@@ -1,3 +1,32 @@
+<?php
+include_once "Database.php";
+
+$db = new Database();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// Put the values in separate variables
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+// Basic Validation, check null
+    if (empty($email) || empty($password)) {
+        if (empty($email)) {
+            $errors[] = "Email is required.";
+        }
+        if (empty($password)) {
+            $errors[] = "Password must be filled in.";
+        }
+    } else { // Start verify the identity
+        $db = new Database();
+        $password_from_db = $db->getPassword($email);
+        if (password_verify($password, $password_from_db)) {
+            echo '<script>alert("Your password is correct")</script>';
+        } else {
+            echo '<script>alert("Your password is wrong")</script>';
+        }
+        $db->close();
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php
@@ -7,16 +36,17 @@ include 'head.php';
 <body>
 <?php include 'navbar.php' ?>
 <div class="container">
-    <form>
-        <h1 class="text-center"><?php echo $section_name ?></h1>
+    <form method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <h1 class="text-center"><?= $section_name ?></h1>
+
         <div class="form-group">
             <label for="inputEmail">Email address</label>
-            <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp"
+            <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" name="email"
                    placeholder="Enter email">
         </div>
         <div class="form-group">
             <label for="inputPassword">Password</label>
-            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+            <input type="password" class="form-control" id="inputPassword" placeholder="Password" name="password">
         </div>
         <div id="login-pg-opt" class="justify-content-between">
             <div class="form-group form-check d-inline-block">
