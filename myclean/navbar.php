d69c558a05a8ddda011f2e0188e67b7cd461848a
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-15 14:36:54 +1000

    Implement with role basis visibility

--- myclean/navbar.php
+++ myclean/navbar.php
@@ -1,5 +1,8 @@
 <?php
 include_once 'head.php';
+$hideNavBarPages = array(
+    "login.php"
+)
 ?>
 <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
     <div class="container-fluid">
@@ -18,32 +21,38 @@ include_once 'head.php';
 
 
             </ul>
-            <!--Check whether it is logged in, then decide showing or not-->
-            <?php if (!isset($_SESSION['role'])) { ?>
-                <ul class="navbar-nav" id="navGroupRight">
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.php">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="register.php">Signup</a>
-                    </li>
-                </ul>
-            <?php } else { ?>
-                <ul class="navbar-nav" id="navGroupRight">
-                    <li class="nav-item dropdown navbar-text">
+            <?php
+            if (!in_array(basename($_SERVER['PHP_SELF']), $hideNavBarPages)) {
+                ?>
+                <!--Check whether it is logged in, then decide showing or not-->
+                <?php if (
+                    !isset($_SESSION['role'])
+                ) { ?>
+                    <ul class="navbar-nav" id="navGroupRight">
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="register.php">Signup</a>
+                        </li>
+                    </ul>
+                <?php } else { ?>
+                    <ul class="navbar-nav" id="navGroupRight">
+                        <li class="nav-item dropdown navbar-text">
                         <span class="dropdown-toggle" id="UsernameDropdown" role="button" data-bs-toggle="dropdown"
                               aria-expanded="false">Hello, <?= $_SESSION['name'] ?></span>
-                        <ul class="dropdown-menu" aria-labelledby="UsernameDropdown">
-                            <li><a class="dropdown-item" href="#">Dashboard</a></li>
-                            <li><a class="dropdown-item" href="#">Order</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="logout.php">Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <?php
+                            <ul class="dropdown-menu" aria-labelledby="UsernameDropdown">
+                                <li><a class="dropdown-item" href="#">Dashboard</a></li>
+                                <li><a class="dropdown-item" href="#">Order</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <?php
+                }
             }
             ?>
         </div>
