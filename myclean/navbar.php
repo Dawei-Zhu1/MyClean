5ef4f0275be2645ee36c3aeb9b4e875c54e2d9b6
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-15 14:27:26 +1000

    Update with better session handling

--- myclean/navbar.php
+++ myclean/navbar.php
@@ -1,15 +1,52 @@
-<nav class="navbar sticky-top navbar-light bg-light">
-    <div class="container">
+<?php
+include_once 'head.php';
+?>
+<nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+    <div class="container-fluid">
         <a class="navbar-brand" href="#">
             <img src="./assets/img/myclean-logo.svg" alt="MyClean Logo" width="120" height="50">
         </a>
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="/login">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="register.php">Signup</a>
-            </li>
-        </ul>
+
+        <div class="collapse navbar-collapse d-flex" id="navbarItemsRight">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Feature</a>
+                </li>
+
+
+            </ul>
+            <!--Check whether it is logged in, then decide showing or not-->
+            <?php if (!isset($_SESSION['role'])) { ?>
+                <ul class="navbar-nav" id="navGroupRight">
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Signup</a>
+                    </li>
+                </ul>
+            <?php } else { ?>
+                <ul class="navbar-nav" id="navGroupRight">
+                    <li class="nav-item dropdown navbar-text">
+                        <span class="dropdown-toggle" id="UsernameDropdown" role="button" data-bs-toggle="dropdown"
+                              aria-expanded="false">Hello, <?= $_SESSION['name'] ?></span>
+                        <ul class="dropdown-menu" aria-labelledby="UsernameDropdown">
+                            <li><a class="dropdown-item" href="#">Dashboard</a></li>
+                            <li><a class="dropdown-item" href="#">Order</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <?php
+            }
+            ?>
+        </div>
+
     </div>
 </nav>
