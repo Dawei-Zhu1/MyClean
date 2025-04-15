0c5a946134518b9ad7ad48a586d1b1cfd6375afb
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-15 14:57:19 +1000

    Add bootstrap icon

--- myclean/head.php
+++ myclean/head.php
@@ -4,10 +4,14 @@ require_once 'config.php';
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--CSS-->
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!--Local CSS-->
     <link rel="stylesheet" href="/myclean/assets/stylesheets/style.css">
+
     <title><?php echo "$section_name - MyClean" ?? 'MyClean'; ?></title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
