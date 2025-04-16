23dd4b59ab7dcee56d31a4588d9ead89e1184ef8
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-16 14:49:29 +1000

    Add the item

--- myclean/navbar.php
+++ myclean/navbar.php
@@ -23,6 +23,9 @@ $icons = array(
         <div class="collapse navbar-collapse d-flex" id="navbarItemsRight">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
+                    <a class="nav-link" href="index.php">Home</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="about.php">About Us</a>
                 </li>
                 <li class="nav-item">
