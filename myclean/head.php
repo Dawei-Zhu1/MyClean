27f83f1afc3b1ef5f9a7c07f7c0cf11532c885a1
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-16 14:48:28 +1000

    Remove redundancy

--- myclean/head.php
+++ myclean/head.php
@@ -10,7 +10,7 @@ require_once 'config.php';
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!--Local CSS-->
-    <link rel="stylesheet" href="/myclean/assets/stylesheets/style.css">
+    <link rel="stylesheet" href="assets/stylesheets/style.css">
     <!--More Style Sheet-->
     <?= $stylesheet ?? ''; ?>
 
