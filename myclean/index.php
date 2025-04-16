5ae6e4f510ff12ca0a05567a37fbd877e2687f65
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-16 10:35:01 +1000

    Change to include_once

--- myclean/index.php
+++ myclean/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'common/head.php' ?>
+<?php include_once 'head.php' ?>
 <body>
-<?php include 'common/navbar.php' ?>
+<?php include_once 'navbar.php' ?>
 <div class="container">
-    <?php phpinfo() ?>
+    <div></div>
 </div>
 </body>
 </html>
