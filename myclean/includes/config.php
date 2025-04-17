e4a58400773b62a667fd92b9d8fc21a5d723cd27
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-17 22:20:58 +1000

    Add config.php

---
+++ myclean/includes/config.php
@@ -0,0 +1,9 @@
+<?php
+//Define the base location
+const SITE_NAME = 'MyClean';
+//DB details
+const DB_HOST = 'db-myclean.cj8k0cuo8rz4.ap-southeast-2.rds.amazonaws.com';
+const DB_NAME = 'MYCLEANDB';
+const DB_USER = 'admin';
+const DB_PASS = 'Success+3407';
+
