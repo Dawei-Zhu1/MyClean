9953c0a1e1e3d72663f512a517ea5a319cfe3f59
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-17 15:54:35 +1000

    Refactor btn_book_a_service.php

--- myclean/btn_book_a_service.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (basename($_SERVER['PHP_SELF']) == 'btn_book_a_service.php') {
-    include_once 'head.php';
-}
-?>
-<a href="login.php" class="floating-book-btn">Book a Service</a>
---
+++ myclean/includes/btn_book_a_service.php
@@ -0,0 +1,6 @@
+<?php
+if (basename($_SERVER['PHP_SELF']) == 'btn_book_a_service.php') {
+    include_once __DIR__.'/head.php';
+}
+?>
+<a href="../pages/login.php" class="floating-book-btn">Book a Service</a>
