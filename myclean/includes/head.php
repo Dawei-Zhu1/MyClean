8beaf617494553dce0daf3b64538efa6a57ccff5
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-19 14:00:30 +1000

    Modify the Relocated link

--- myclean/includes/head.php
+++ myclean/includes/head.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/config.php';
+require_once __DIR__ . '/config.php';
 ?>
 <head>
     <meta charset="utf-8">
@@ -10,11 +10,11 @@ require_once __DIR__.'/config.php';
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <!--Local CSS-->
-    <link rel="stylesheet" href="../assets/stylesheets/style.css">
+    <link rel="stylesheet" href="/assets/stylesheets/style.css">
     <!--More Style Sheet-->
     <?= $stylesheet ?? ''; ?>
 
-    <title><?= (isset($section_name) ? $section_name . " - " : '').SITE_NAME; ?></title>
+    <title><?= (isset($section_name) ? $section_name . " - " : '') . SITE_NAME; ?></title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
             crossorigin="anonymous"></script>
