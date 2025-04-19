134c3ca19839a677643fd6df1c0edd083e6418ac
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-19 14:00:42 +1000

    Modify the Relocated link

--- myclean/includes/navbar.php
+++ myclean/includes/navbar.php
@@ -17,23 +17,23 @@ $icons = array(
 ?>
 <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="../index.php">
+        <a class="navbar-brand" href="/index.php">
             <img src="/assets/img/myclean-logo.svg" alt="MyClean Logo" width="120" height="50">
         </a>
 
         <div class="collapse navbar-collapse d-flex" id="navbarItemsRight">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="../index.php">Home</a>
+                    <a class="nav-link" href="/index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../pages/about.php">About Us</a>
+                    <a class="nav-link" href="/pages/about.php">About Us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../pages/services.php">Services</a>
+                    <a class="nav-link" href="/pages/services.php">Services</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../pages/contact.php">Contact</a>
+                    <a class="nav-link" href="/pages/contact.php">Contact</a>
                 </li>
 
 
@@ -47,10 +47,10 @@ $icons = array(
                         !$_SESSION['logged_in']
                     ) { ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="../pages/login.php">Login</a>
+                            <a class="nav-link" href="/pages/auth/login.php">Login</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../pages/register.php">Signup</a>
+                            <a class="nav-link" href="/pages/auth/register.php">Signup</a>
                         </li>
 
                         <?php
@@ -70,7 +70,7 @@ $icons = array(
                                 <li>
                                     <hr class="dropdown-divider">
                                 </li>
-                                <li><a class="dropdown-item" href="../pages/logout.php">Logout</a></li>
+                                <li><a class="dropdown-item" href="/pages/auth/logout.php">Logout</a></li>
                             </ul>
                         </li>
 
@@ -80,6 +80,5 @@ $icons = array(
                 ?>
             </ul>
         </div>
-
     </div>
 </nav>
