6e81186da91bf76e720ec0fb0fbb65dc1035329a
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-20 15:56:03 +1000

    Simplify the syntax

--- myclean/includes/btn_book_a_service.php
+++ myclean/includes/btn_book_a_service.php
@@ -4,14 +4,8 @@
 if (basename($_SERVER['PHP_SELF']) == 'btn_book_a_service.php') {
     include_once __DIR__ . '/head.php';
 }
-/*Detect whether the user is logged in*/
-$link = '';
-if (!isset($_SESSION['is_login']) or !$_SESSION['is_login']) {
-    $link = '../pages/auth/login.php';
-} else {
-    $link = '../pages/booking.php';
-}
+/*Detect whether the user is logged in, then decide to direct to login or booking page*/
+$link = (!isset($_SESSION['is_login']) or !$_SESSION['is_login']) ? '../pages/auth/login.php' : '../pages/booking.php';
 ?>
-
 
 <a href='<?= $link ?>' class="floating-book-btn">Book a Service</a>
