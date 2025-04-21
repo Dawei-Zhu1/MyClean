49b9ce031eaba676a72158d287ce3710e37366a0
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-22 00:41:52 +1000

    Add a testing file

---
+++ myclean/test/test_check_acc.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+include_once __DIR__ . '/../includes/check_accessibility.php';
+// Eligible
+echo "test1: 'user' in ['user', 'admin']" . PHP_EOL;
+$result = check_accessibility('user', ['user', 'admin']);
+echo $result?'True':'False';
+// Not eligible
+echo "test2: 'visitor' in ['user', 'admin']" . PHP_EOL;
+$result = check_accessibility('visitor', ['user', 'admin']);
+echo $result?'True':'False';
