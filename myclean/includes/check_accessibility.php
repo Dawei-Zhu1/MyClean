ed491123cad74ed49e940137d16352be67741d28
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-22 01:05:52 +1000

    Remove session

--- myclean/includes/check_accessibility.php
+++ myclean/includes/check_accessibility.php
@@ -1,11 +1,10 @@
 <?php
-session_start();
 /** Check whether current role is eligible to gain access
- * @param string $role  role of current user
+ * @param string $role role of current user
  * @param array $required_roles required role
  * @return bool
  */
-function check_accessibility(string $role, array $required_roles): bool
+function check_accessibility(string $role, array $required_roles = ['user', 'admin']): bool
 {
     return in_array($role, $required_roles);
 }
