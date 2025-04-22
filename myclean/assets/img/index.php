4cdba12f35cb86fb3d3d178d9da33623a7be324b
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-22 13:57:15 +1000

    Add indices to prevent the directory being accessed directly

---
+++ myclean/assets/img/index.php
@@ -0,0 +1,8 @@
+<!-- /var/www/html/test/index.html -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>403</title></head>
+<body><h1>403 Forbidden</h1></body>
+</html>
---
+++ myclean/pages/index.php
@@ -0,0 +1,4 @@
+<!-- /var/www/html/test/index.html -->
+<!DOCTYPE html>
+<html><head><meta charset="UTF-8"><title>403</title></head>
+<body><h1>403 Forbidden</h1></body></html>
