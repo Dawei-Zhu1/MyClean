402aa9c2566633a274cfab06e58ad4b5237c0ebd
Dawei-Zhu1 <144910267+Dawei-Zhu1@users.noreply.github.com>
2025-04-24 10:28:54 +1000

    Improve admin series function

--- myclean/admin.php
+++ myclean/admin.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
+
 include_once __DIR__ . '/includes/config.php';
 include_once __DIR__ . '/includes/session.php';
+include_once __DIR__ . '/includes/check_accessibility.php';
 $_SESSION['redirect_after_login'] = $_SERVER['REQUEST_URI'];
 ?>
 <!DOCTYPE html>
@@ -9,11 +11,12 @@ $_SESSION['redirect_after_login'] = $_SERVER['REQUEST_URI'];
 <?php $section_name = 'Administration Page';
 include_once __DIR__ . '/includes/head.php' ?>
 <body>
-<?php include_once __DIR__ . '/includes/navbar.php' ?>
+<?php include_once __DIR__ . '/includes/navbar.php'; ?>
 <div class="main-layout">
     <?php if (isset($_SESSION['role']) and check_accessibility($_SESSION['role'], ADMIN_GROUP)) : ?>
-        <h1>Page is being built</h1>
+
+        <h1>You're in. But the page is being built</h1>
     <?php else: ?>
-        <?php include_once __DIR__ . '/pages/access_deny.php' ?>
+        <?php include_once './pages/access_deny.php' ?>
     <?php endif; ?>
 </div>
--- myclean/includes/config.php
+++ myclean/includes/config.php
@@ -6,5 +6,5 @@ const DB_HOST = 'db-myclean.cj8k0cuo8rz4.ap-southeast-2.rds.amazonaws.com';
 const DB_NAME = 'MYCLEANDB';
 const DB_USER = 'admin';
 const DB_PASS = 'Success+3407';
-const ADMIN_GROUP = ['admin'];
-const ALL_GROUPS = ['admin', 'user'];
+const ADMIN_GROUP = [2];
+const ALL_GROUPS = [2, 1];
